07622a0de50e52db899af94dab8f59259512d22c
Dimon <95857813+slimief@users.noreply.github.com>
2022-12-02 23:35:10 +0300

    Update 9lab.php

--- 9lab.php
+++ 9lab.php
@@ -1,7 +1,7 @@
 <?php
     $fh = fopen('laba9.txt','a+'); 
     while ($line = fgets($fh)) { 
-        $line = "192.255.25.0";
+        $line = "192.255.255.0";
     $tmp = explode("|", $line); 
 ?>
 <div>
