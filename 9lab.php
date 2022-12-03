03d6874b65ea41b5129fe8844c507a581b136b72
Dimon <95857813+slimief@users.noreply.github.com>
2022-12-03 11:58:58 +0300

    Update 9lab.php

--- 9lab.php
+++ 9lab.php
@@ -1,13 +1,11 @@
 <?php
-    $fh = fopen('laba9.txt','a+'); 
-    while ($line = fgets($fh)) { 
-        $line = "192.255.255.0";
-    $tmp = explode("|", $line); 
-?>
-<div>
-    <?= $tmp[0]?> 
-</div>
-
-<?php } 
-    fclose($fh);
+    $text = fopen('laba9.txt','r');
+    while ($line = fgets($text)) { 
+        $a = '/\b((([1-9])|([1-9][0-9])|(1[0-9][0-9])|(2[0-5][0-5]))\.(\d|([1-9][0-9])|(1[0-9][0-9])|(2[0-5][0-5]))\.(\d|([1-9][0-9])|(1[0-9][0-9])|(2[0-5][0-5]))\.((2[0-5][0-5])|(1[0-9][0-9])|([1-9][0-9])|\d))\b/';
+        preg_match_all($a ,$line, $massiv);
+        foreach($massiv[0] as $b){
+            echo $b."</br>";
+        }       
+    } 
+    fclose($text);
 ?>
